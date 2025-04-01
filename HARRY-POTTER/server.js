9dd439a31cd38bb2244db4680532331509724d92
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-01 11:04:20 -0300

    finish

--- HARRY-POTTER/server.js
+++ HARRY-POTTER/server.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const wizardRoutes = require("./src/routes/wizardRoutes");
-//const houseRoutes = require("./src/routes/houseRouter");
+const houseRoutes = require("./src/routes/houseRouter");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", wizardRoutes);
-//app.use("/api", houseRoutes);
+app.use("/api", houseRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
